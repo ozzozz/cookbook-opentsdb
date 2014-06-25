b884c599207e4c511afdf17fd95267337d59536f
ozzozz <ozzozz@gmail.com>
2014-06-25 14:32:40 +0900

    remove debian support from matadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ description      'Installs/Configures opentsdb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.4'
 
-supports 'debian', '= 6'
 supports 'ubuntu', '= 10.04'
 supports 'ubuntu', '= 12.04'
+supports 'ubuntu', '= 14.04'
 
 depends 'apt',  '~> 2.3.0'
 depends 'java', '~> 1.21.0'
