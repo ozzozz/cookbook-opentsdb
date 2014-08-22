836f1e33d667769f1063e696b747ce044da3521c
ozzozz <ozzozz@gmail.com>
2014-08-22 11:25:23 +0900

    Revert "bump version to 0.2.5"
    This reverts commit 2705eff9bbad14793d0407aaa7c7dd63011f1e43.

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'opentsdb'
-maintainer       'ozzozz'
-maintainer_email 'cookbooks@kn.ozz.in'
+maintainer       'whoop, Inc.'
+maintainer_email 'benesch@whoop.com'
 license          'All rights reserved'
 description      'Installs/Configures opentsdb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.5'
+version          '0.2.4'
 
 supports 'ubuntu', '= 10.04'
 supports 'ubuntu', '= 12.04'
